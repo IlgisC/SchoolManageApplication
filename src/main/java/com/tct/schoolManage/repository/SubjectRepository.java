10ba45c84ce01d8950b02c5eb56114394f51d419
IlgisC <gisi.cehu05@gmail.com>
2025-10-05 01:56:47 +0200

    first commit

---
+++ src/main/java/com/tct/schoolManage/repository/SubjectRepository.java
@@ -0,0 +1,7 @@
+package com.tct.schoolManage.repository;
+
+import com.tct.schoolManage.entity.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubjectRepository extends JpaRepository<Subject, Integer>{
+}
