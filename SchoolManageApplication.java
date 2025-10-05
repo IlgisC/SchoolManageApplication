105a03ae23ec6d44488e65171d532e93092afd29
IlgisC <148154015+IlgisC@users.noreply.github.com>
2025-10-05 02:43:40 +0200

    Add files via upload

---
+++ SchoolManageApplication.java
@@ -0,0 +1,14 @@
+package com.tct.schoolManage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManageApplication.class, args);
+		System.out.println("Welcome Bederista");
+	}
+
+}
